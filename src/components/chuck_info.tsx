dec357c8868effdd651694dfd5ea1a501f3ba5b6
Nigel Faria <nigel.faria@gmail.com>
2023-01-31 16:11:24 +0000

    Requirement 2: Displaying whales saved & roundhouse kicks

--- src/components/chuck_info.tsx
+++ src/components/chuck_info.tsx
@@ -1,26 +1,16 @@
-const ChuckInfo = () => {
 
-	// 💡 note that this component uses an "explicit return"
-	// Compare to <ChuckCard/> which just returns the JSX directly like this:
-	// const ChuckCard = () => <>... some JSX Here...</>;
+interface ChuckInfoProps {
+	whalesSaved: number;
+	roundHouseKicks: number;
+}
 
-	// instead, this component uses curly braces `{}` and a `return` statement
-
-	// this allows us to write code here to perform some logic before we construct the JSX
-	// we want to return
-
-	// if you don't have any particular need to write logic in a component, 
-	// then it's cleaner to use the implicit return style from <ChuckCard/>
-	// otherwise you can use the curly braces and return to write code first
-
-	const someVariable = ""; // you don't need this to finish this exercise, it's just to demo JS here
-
+const ChuckInfo:React.FC<ChuckInfoProps> = ({whalesSaved,roundHouseKicks}) => {
 
 	return (
 		<>
-			<p>Number of Whales Saved: </p>
+			<p>Number of Whales Saved: {whalesSaved}</p>
 
-			<p>Number of Round House Kicks (in the last day): </p>
+			<p>Number of Round House Kicks (in the last day): {roundHouseKicks} </p>
 		</>);
 }
 
